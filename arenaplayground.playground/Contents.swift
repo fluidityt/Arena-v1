243f2c3311551db603515b86d022b166c97c855f
dudeguy <3403>
2016-09-23 19:47:16 -0400

    gettin funcy w the globes

--- arenaplayground.playground/Contents.swift
+++ arenaplayground.playground/Contents.swift
@@ -1,14 +1,69 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
+print("PG Executing... \n")
+// Test
 
-var str = "Hello, playground"
+/*
+	struct definition -> game data definition -> sp definition
 
-let z:String = {
-	Logic: do {
-		if 1==2 { return "hi" }
-		else { return "lol" }
-		}
-	}()
+	on entry:
+		1. SP holds one instance of GD
+		2. GD holds one instance of each GS
+*/
 
-print (z)
+struct GameStructs {
+	// Player stuff
+	struct Player {
+		
+		let age:	Int
+		let name: String
+	}
+	
+	
+	// UX stuff:
+	struct UX {
+		
+		let currentY:	Int
+		let previousY: Int
+	}
+	
+}; typealias GS = GameStructs
+
+// data
+struct GameData {
+	
+	// All my data:
+	let
+	player_data: GS.Player,
+	ux_data: GS.UX
+	
+}; typealias GD = GameData
+
+// shared
+class SharedPrefs {
+	
+	static var data: GameData?
+	
+	static let description = { print (SharedPrefs.data!) }
+}; typealias SP = SharedPrefs
+
+// Quick updater:
+// make it init?;
+// Entry
+initStuff: do {
+	
+	let new_player = GS.Player (age: 22, name: "max")
+	let new_ux	= GS.UX (currentY: 1, previousY: 2)
+	
+	let newgame = GameData (player_data: new_player, ux_data: new_ux)
+	
+	SharedPrefs.data = newgame
+	SP.description()
+}
+
+// first interaction
+testStuff: do {
+
+
+}
