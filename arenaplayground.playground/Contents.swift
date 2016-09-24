e054e33be0e471076fb4d2aec655c801c56cc547
dudeguy <3403>
2016-09-23 21:34:42 -0400

    struccy

--- arenaplayground.playground/Contents.swift
+++ arenaplayground.playground/Contents.swift
@@ -1,17 +1,18 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
-print("PG Executing... \n")
+print("*****  PG Executing...  *****\n")
 // Test
 
 /*
-	struct definition -> game data definition -> sp definition
+struct definition -> game data definition -> sp definition
 
-	on entry:
-		1. SP holds one instance of GD
-		2. GD holds one instance of each GS
+on entry:
+1. SP holds one instance of GD
+2. GD holds one instance of each GS
 */
 
+// struct
 struct GameStructs {
 	// Player stuff
 	struct Player {
@@ -28,42 +29,77 @@ struct GameStructs {
 		let previousY: Int
 	}
 	
+	struct Item {
+		let name: String
+	}
+	
 }; typealias GS = GameStructs
+
 
 // data
 struct GameData {
 	
-	// All my data:
+	// All my data... would be easy as var... lol
 	let
 	player_data: GS.Player,
-	ux_data: GS.UX
+	ux_data: GS.UX,
+	item_list: [GS.Item]
 	
+	
+	// There is an extended init below SharedPrefs...
+	// Didn't put here for procedural reasons...
 }; typealias GD = GameData
 
-// shared
+
+// Only
 class SharedPrefs {
 	
+	// Data stuff..
 	static var data: GameData?
 	
+	// Other stuff...
 	static let description = { print (SharedPrefs.data!) }
 }; typealias SP = SharedPrefs
 
-// Quick updater:
-// make it init?;
+
+// Extended to use SP and for copying / update ease
+extension GameData {
+	
+	// Use this init to make a copy... pretty much only used in next func
+	// (can't declare in func because fail)
+	init(		updated_player pd: GS.Player = SP.data!.player_data,
+	     		               updated_ux uxd: GS.UX = SP.data!.ux_data,
+	     		                          updated_items itm: [GS.Item] = SP.data!.item_list) {
+		
+		iterateSelf: do {
+			// Iterate through...
+			self.player_data = pd
+			self.ux_data = uxd
+			self.item_list = itm
+		}
+	}
+}
+
+
 // Entry
 initStuff: do {
 	
 	let new_player = GS.Player (age: 22, name: "max")
 	let new_ux	= GS.UX (currentY: 1, previousY: 2)
+	let items = GS.Item(name: "candy")
 	
-	let newgame = GameData (player_data: new_player, ux_data: new_ux)
+	let new_game = GameData (player_data: new_player, ux_data: new_ux, item_list: [items])
 	
-	SharedPrefs.data = newgame
-	SP.description()
+	SharedPrefs.data = new_game
+	SharedPrefs.description()
 }
+
 
 // first interaction
 testStuff: do {
-
-
+	
+	
 }
+
+
+print("\n***** PG Success!! *******")
