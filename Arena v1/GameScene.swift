0c7a9a3c866c7dab5a6d5ab44cfc6afa4cbc0b95
dudeguy <3403>
2016-10-01 16:30:53 -0400

    z

--- Arena v1/GameScene.swift
+++ Arena v1/GameScene.swift
@@ -6,9 +6,14 @@
 //  Copyright (c) 2016 Dude Guy . All rights reserved.
 
 // _ means global
+
 import SpriteKit
 
-protocol Static {}
+
+
+// fun
+protocol Static {
+}
 
 /* Shortcuts */
 typealias G 	= Global
@@ -19,7 +24,8 @@ typealias XY 	= G.XnY
 typealias Sanity = G.Funcs.Sanity
 
 // Typealias
-func notafunc_conventions() {
+func notafunc_conventions () {
+
 /*
 
 func purFunc(mat:Void) -> String {
@@ -36,137 +42,136 @@ something = purFunc(d)
 
 class GameScene: SKScene {
 
+  override func didMoveToView (view: SKView) {
 
-	override func didMoveToView(view: SKView) {
-		
-		genericInits: do {
-			
-			Global.SELF = self
-			XY.CENTER_SCREEN = CGPoint(x:CGRectGetMidX(G.SELF.frame),
-			                           y:CGRectGetMidY(G.SELF.frame))
-		}
-		
-		initCircles: do {
-			
-			Global.Nodes.central = Circle()
-			G.SELF.addChild(N.central)
-			
-			N.central.runAction(SKAction.rotateToAngle(0, duration: 0.5))
-			G.Angles.super_angle = N.central.zRotation
-			G.Angles.angle.current = G.Angles.super_angle
-}
-		return // from DMtV
-	}
-	
-	
-	
-	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-		for touch in touches {
-			
-			// The time NOW (basically) when started TB
-			Global.time2.atBegan  = G.time.current
+    genericInits:do {
 
-			// The next input (TM) will be our 'first drag' | error checking is good!
-			Global.first_drag == false ? G.first_drag = true : printd("first drag was f\\ked")
-		
-			// Where we just clicked is our new y.current
-			Global.XnY.y.current = Sanity.updateCurrentY( touch.locationInNode(self).y)
-		}
-		
-		
-		return // from TB
-	}
-	
-	
-	
-	override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-		for touch in touches {
-		
-			updateGlobesWithNewInfo: do {
-				
-				XY.y.previous = Sanity.updatePreviousY(currentY: XY.y.current)
-				XY.y.current  = Sanity.updateCurrentY (touch.locationInNode(self).y)
-			
-				if G.first_drag == true {
-				
-					toggle(&G.first_drag)		 // This has to be false after first_entry
-				
-					G.time.previous 	= G.time.first // time.atTB compared with time.atMove
-				}
-			}
-				
-		
-			// Paused (lateral)--no angle now--reset values
-			guard (XY.y.current != XY.y.previous) else {
-				
-				printl("paused but didn't release")
-					N.central.removeAllActions()
-						XY.y_tuple = ("refreshed", 0,0,0)
-				
-				return // From guard
-			}
-			
-			Hotfix() {
-				
-				if XY.y.current > XY.y.previous {
-					A.angle.next = A.angle.current + 0.5
-					N.central.runAction(SKAction.rotateToAngle(A.angle.next, duration: 0.0))
-					A.angle.current = A.angle.next
-				}
-				else {
-					A.angle.next = A.angle.current - 0.5
-					N.central.runAction(SKAction.rotateToAngle(A.angle.next, duration: 0.0))
-					A.angle.current = A.angle.next
-				}
-			}
-			
-			return
-			
+      Global.SELF = self
+      XY.CENTER_SCREEN = CGPoint (x: CGRectGetMidX (G.SELF.frame),
+                                  y: CGRectGetMidY (G.SELF.frame))
+    }
 
-			;
-			let fully_handled_rotation_action_with_acceleration_and_smoothing
-				= findRotationAction(XY.y.current)
-			;
-			
-				runFoundAction: do {
-				N.central.runAction(fully_handled_rotation_action_with_acceleration_and_smoothing)
-				}
-				
-			return // From TM
-			}
-		}
-	
-	override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-					XY.y_tuple = ("refreshed", 0,0,0)
-	}
-	
-	
-	
-	override func update(currentTime: CFTimeInterval) {
-		
-		updateClock: do {
-			/*
-			// Clocker...
-			_clock_count += 1
-			
-			// Seconder...
-			if _clock_count == 60 {
-				
-				_clock_count = 0
-				_seconds += 1
-			}
-			
-			// Spawner...
-			if _seconds == 3 {
-				
-				enemySpawner()
-				_seconds = 0
-			}
-			
-			
-			*/
-		}
-		
+    initCircles:do {
+
+      Global.Nodes.central = Circle ()
+      G.SELF.addChild (N.central)
+
+      N.central.runAction (SKAction.rotateToAngle (0, duration: 0.5))
+      G.Angles.super_angle = N.central.zRotation
+      G.Angles.angle.current = G.Angles.super_angle
+    }
+    return // from DMtV
+  }
+
+
+  override func touchesBegan (touches: Set<UITouch>, withEvent event: UIEvent?) {
+
+    for touch in touches {
+
+      // The time NOW (basically) when started TB
+      Global.time2.atBegan = G.time.current
+
+      // The next input (TM) will be our 'first drag' | error checking is good!
+      Global.first_drag == false ? G.first_drag = true : printd ("first drag was f\\ked")
+
+      // Where we just clicked is our new y.current
+      Global.XnY.y.current = Sanity.updateCurrentY (touch.locationInNode (self).y)
+    }
+
+
+    return // from TB
+  }
+
+
+  override func touchesMoved (touches: Set<UITouch>, withEvent event: UIEvent?) {
+
+    for touch in touches {
+
+      updateGlobesWithNewInfo:do {
+
+        XY.y.previous = Sanity.updatePreviousY (currentY: XY.y.current)
+        XY.y.current = Sanity.updateCurrentY (touch.locationInNode (self).y)
+
+        if G.first_drag == true {
+
+          toggle (&G.first_drag)     // This has to be false after first_entry
+
+          G.time.previous = G.time.first // time.atTB compared with time.atMove
+        }
+      }
+
+
+      // Paused (lateral)--no angle now--reset values
+      guard (XY.y.current != XY.y.previous) else {
+
+        printl ("paused but didn't release")
+        N.central.removeAllActions ()
+        XY.y_tuple = ("refreshed", 0, 0, 0)
+
+        return // From guard
+      }
+
+      Hotfix () {
+
+        if XY.y.current > XY.y.previous {
+          A.angle.next = A.angle.current + 0.5
+          N.central.runAction (SKAction.rotateToAngle (A.angle.next, duration: 0.0))
+          A.angle.current = A.angle.next
+        }
+        else {
+          A.angle.next = A.angle.current - 0.5
+          N.central.runAction (SKAction.rotateToAngle (A.angle.next, duration: 0.0))
+          A.angle.current = A.angle.next
+        }
+      }
+
+      return
+
+
+      ;
+      let fully_handled_rotation_action_with_acceleration_and_smoothing
+      = findRotationAction (XY.y.current)
+      ;
+
+      runFoundAction:do {
+        N.central.runAction (fully_handled_rotation_action_with_acceleration_and_smoothing)
+      }
+
+      return // From TM
+    }
+  }
+
+  override func touchesEnded (touches: Set<UITouch>, withEvent event: UIEvent?) {
+
+    XY.y_tuple = ("refreshed", 0, 0, 0)
+  }
+
+
+  override func update (currentTime: CFTimeInterval) {
+
+    updateClock:do {
+      /*
+      // Clocker...
+      _clock_count += 1
+
+      // Seconder...
+      if _clock_count == 60 {
+
+        _clock_count = 0
+        _seconds += 1
+      }
+
+      // Spawner...
+      if _seconds == 3 {
+
+        enemySpawner()
+        _seconds = 0
+      }
+
+
+      */
+    }
+
 //		
 //		A.angle.next = A.angle.current + 0.25
 //		
@@ -175,13 +180,14 @@ class GameScene: SKScene {
 //
 //		G.time.current = currentTime
 //		
-		
-	}//updat	
-}//EoC
 
-func shapeTex(node:SKShapeNode) {
-	
-	node.fillColor = UIColor.whiteColor()
-	node.fillTexture = SKTexture.init(image: UIImage(named: "twistycenter")!)
+  }//updat
+}
+//EoC
+
+func shapeTex (node: SKShapeNode) {
+
+  node.fillColor = UIColor.whiteColor ()
+  node.fillTexture = SKTexture.init (image: UIImage (named: "twistycenter")!)
 }
 
--- Arena v1/GameStructs.swift
+++ Arena v1/GameStructs.swift
@@ -10,95 +10,96 @@
 import SpriteKit
 
 struct Global: Static {
-	
-	typealias CFTI = CFTimeInterval
-	typealias CGF = CGFloat
-	typealias YValue = CGFloat // Cause we use it a lot
-	
-	
-	/// LET CONSTANTS :d
-	struct Config: Static {
-		static let
-		verbose 	= false,
-		exec 			= false,
-		
-		real_jump = CGFloat(20),
-		
-		accel_strength = CGFloat(0.001),
-		speed = (min: CGFloat(0.01),								// The min/max speed is 100/400 PPS
-			max: CGFloat(0.4))									// ...which is distance actuall
-		;
-	}
+
+  typealias CFTI = CFTimeInterval
+  typealias CGF = CGFloat
+  typealias YValue = CGFloat // Cause we use it a lot
 
 
-	/* Random shit that doesn't
-	fit anywhere else */
-	static var
-		SELF = SKScene(),
-		first_drag   = Bool(),
-	
-		/// Maybe delete this
-		time = (first:    CFTimeInterval(),
-						previous: CFTimeInterval(),
-						current:  CFTimeInterval()),
-		
-		/// Maybe refactor this to time
-		time2 = (atBegan: CFTI(), atMove: CFTI(), current: CFTI())
-	
-	;
-	
-	struct Nodes: Static {
-		
-		static var
-			central = Circle()
-		;
-	}
-	
-	struct Angles: Static {
-		static var
-			angle = (current: CGFloat(),
-							 previous: CGFloat(),
-							 next: CGFloat()),
-			
-			super_angle = CGFloat()
-		;
-	}
-	
-	struct XnY: Static {
+  /// LET CONSTANTS :d
+  struct Config: Static {
+    static let
+    verbose        = false,
+    exec           = false,
 
-		typealias XY = Global.XnY
-		
-		static var
-			CENTER_SCREEN = CGPoint(),
-			
-			// For smoothing FIXME: Make tuple
-			y1 = CGFloat(),		y2 = CGFloat(),		y3 = CGFloat(),
-			/// The string, or y_tuple.0 is for INSANITY reasons
-			y_tuple 	= ("tuple", XY.y1, XY.y2, XY.y3),
-			
-			y = (first: CGF(),
-					 previous: CGF(),
-					 current: CGF())
-		;
-	}
-	
-	/// Cuz I like to get Funcy
-	struct Funcs: Static {
-		
-		/// Useless but sane functions
-		struct Sanity: Static {
-			
-			/// For sanity reasons of reassigning Gprevy
-			static func updatePreviousY(currentY not_current_y: YValue) -> YValue {
-				return not_current_y
-			}
-			
-			/// for sanity reasons of updatin G currenty
-			static func updateCurrentY(new_y_coords: YValue) -> YValue {
-				return new_y_coords
-			}
-		}
-	}
+    real_jump      = CGFloat (20),
+
+    accel_strength = CGFloat (0.001),
+    speed          = (min: CGFloat (0.01), // The min/max speed is 100/400 PPS
+                      max: CGFloat (0.4))
+    // ...which is distance actuall
+  }
+
+
+  /* Random shit that doesn't
+  fit anywhere else */
+  static var
+  SELF       = SKScene (),
+  first_drag = Bool (),
+
+  /// Maybe delete this
+  time       = (first: CFTimeInterval (),
+                previous: CFTimeInterval (),
+                current: CFTimeInterval ()),
+
+  /// Maybe refactor this to time
+  time2      = (atBegan: CFTI (), atMove: CFTI (), current: CFTI ())
+;
+
+  struct Nodes: Static {
+
+    static var
+    central = Circle ()
+  ;
+  }
+
+  struct Angles: Static {
+    static var
+    angle       = (current: CGFloat (),
+                   previous: CGFloat (),
+                   next: CGFloat ()),
+
+    super_angle = CGFloat ()
+  ;
+  }
+
+  struct XnY: Static {
+
+    typealias XY = Global.XnY
+
+    static var
+    CENTER_SCREEN = CGPoint (),
+
+    // For smoothing FIXME: Make tuple
+    y1            = CGFloat (), y2 = CGFloat (), y3 = CGFloat (),
+    /// The string, or y_tuple.0 is for INSANITY reasons
+    y_tuple       = ("tuple", XY.y1, XY.y2, XY.y3),
+
+    y             = (first: CGF (),
+                     previous: CGF (),
+                     current: CGF ())
+  ;
+  }
+
+  /// Cuz I like to get Funcy
+  struct Funcs: Static {
+
+    /// Useless but sane functions
+    struct Sanity: Static {
+
+      /// For sanity reasons of reassigning Gprevy
+      static func updatePreviousY (currentY not_current_y: YValue) -> YValue {
+
+        return not_current_y
+      }
+
+      /// for sanity reasons of updatin G currenty
+      static func updateCurrentY (new_y_coords: YValue) -> YValue {
+
+        return new_y_coords
+      }
+    }
+  }
 }
 
 
